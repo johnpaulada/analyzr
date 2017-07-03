dc417936a991227f3c2854ed7fccd24f6c4ab477
John Paul Ada <johnpaulada@Elie-Mini.local.gap>
2017-07-03 13:49:18 +0800

    Created AnalyzerLayout

--- components/AnalysisListItem.js
+++ components/AnalysisListItem.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { TouchableOpacity, Text } from 'react-native';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { ListItem } from 'react-native-elements';
 
 const AnalysisListItem = props => {
-  return <TouchableOpacity onPress={props.onSelect}>
-    <Text>{props.text}</Text>
-  </TouchableOpacity>
+  return <ListItem
+    containerStyle={styles.container}
+    titleStyle={styles.title}
+    title={props.name}
+    subtitle={props.description}
+    onPress={props.onSelect}
+  />
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignSelf: 'stretch',
+    backgroundColor: '#FAFAFA',
+  },
+  title: {
+    fontWeight: 'bold'
+  }
+});
 
 export default AnalysisListItem;
